614f58336fa0cb0af6ae44ca0cbfb7b74a31ec6f
Žan Pevec <zan@1home.io>
2020-10-05 08:21:00 +0200

    Added session and created base login ui

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuesax from 'vuesax'
 
+import 'vuesax/dist/vuesax.css' // Vuesax styles
 Vue.config.productionTip = false
+
+Vue.use(Vuesax, {
+  // options here
+})
 
 new Vue({
   router,
